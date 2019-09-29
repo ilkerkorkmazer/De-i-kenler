2008e9b6e5ae347f9f4bb61e1cf7d244eb8fd902
ilkerkorkmazer <ilker_black@hotmail.com>
2019-09-29 16:17:32 +0300

    degisken örnekleri

---
+++ Lesson2.py
@@ -0,0 +1,11 @@
+benimAdim = "ilker"
+benimSoyadim="korkmazer"
+print(benimAdim)
+print (benimAdim + " " + benimSoyadim)
+
+
+
+isim="ilker"
+soyisim=" korkmazer"
+fullname= isim + soyisim
+print("kullanici adi soyadi : " + fullname)
