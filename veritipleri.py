2bd76c4c70fe12bd8ab7522cfb65857b04549f8e
ilkerkorkmazer <ilker_black@hotmail.com>
2019-09-29 19:19:02 +0300

    ıslemler

--- veritipleri.py
+++ veritipleri.py
@@ -1,2 +1,11 @@
 
 #disaridn alinman 2 sayinin toplamiyla farkinin biribine bolumunden kalan kactir?
+
+
+
+sayi1=int(input("vizenotu : "))
+sayi2=int(input("finalnotu : "))
+deger1=((sayi1 * 3) / 10)
+deger2=((sayi2 * 7) / 10)
+sonuc=((deger1 + deger2) / 2)
+print ("ortalama : " ,sonuc)
